2616bec50baf30fb0b30bdee7ca7565371683745
parvatigitub <yadavparvati679@gmail.com>
2025-07-25 09:03:36 +0530

    Manually added attendance table creation

---
+++ migrations/versions/06a0fa102c21_manually_create_attendance_table.py
@@ -0,0 +1,24 @@
+"""Manually create attendance table
+
+Revision ID: 06a0fa102c21
+Revises: 
+Create Date: 2025-07-25 09:02:48.158655
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '06a0fa102c21'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
