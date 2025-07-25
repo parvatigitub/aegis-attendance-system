0beb88357044e386047656e0cabe7427028f6f47
parvatigitub <yadavparvati679@gmail.com>
2025-07-25 11:45:04 +0530

    Fix: Removed app.run, prepare for Gunicorn

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-import os
 from app import app
 from flask_migrate import upgrade
 
 with app.app_context():
-    upgrade()  # ✅ Apply migrations when the app starts
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    upgrade()  # Apply all migrations
