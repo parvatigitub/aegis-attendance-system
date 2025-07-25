b98f120de61a50a55656fa1a4bd87c2f7075d097
parvatigitub <yadavparvati679@gmail.com>
2025-07-25 10:33:52 +0530

    Fix: Bind Flask app to 0.0.0.0 for Render deployment

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import os
+
 from app import app
 from flask_migrate import upgrade
 
@@ -5,3 +7,4 @@ if __name__ == '__main__':
     with app.app_context():   # ✅ Ye line application context activate karega
         upgrade()             # ✅ Ye migration apply karega
     app.run()
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
