85e57096f2b815b2c374237bf59759a0f9a57d6e
parvatigitub <yadavparvati679@gmail.com>
2025-07-25 10:25:13 +0530

    Update: Changed DB URI to new Render database

--- config.py
+++ config.py
@@ -4,11 +4,10 @@ class Config:
     SECRET_KEY = 'shivam_attendnce_models'
 
     if os.environ.get("RENDER") == "true":
-        SQLALCHEMY_DATABASE_URI = 'postgresql://root:CGIf9UBARjkPDGij5fLHzIN0Rl2Mmcac@dpg-d1vku6h5pdvs73e84ls0-a/attendance_db_38bl'
+        SQLALCHEMY_DATABASE_URI = 'postgresql://attendance_db_2bae_user:A4GORsQuyhwOsWwV7F8G6gr0gMAmXUS4@dpg-d21gphadbo4c73e57scg-a/attendance_db_2bae'
     else:
         SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/attendance_db'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app', 'static', 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
-
