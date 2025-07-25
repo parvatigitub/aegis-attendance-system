da3691ea6470f25f5553aed10b15bdb48205e781
parvatigitub <yadavparvati679@gmail.com>
2025-07-25 11:39:18 +0530

    Fix: Bind to 0.0.0.0 and added migration on deploy

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import os
 from app import app
 from flask_migrate import upgrade
 
+with app.app_context():
+    upgrade()  # ✅ Apply migrations when the app starts
+
 if __name__ == '__main__':
-    with app.app_context():
-        upgrade()
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
