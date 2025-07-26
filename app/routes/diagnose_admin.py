c40b1c36b06d3adf7ceb3fb95ec4c44cbfdcb3a0
parvatigitub <yadavparvati679@gmail.com>
2025-07-26 10:38:34 +0530

    Fix diagnose_admin route

--- app/routes/diagnose_admin.py
+++ app/routes/diagnose_admin.py
@@ -1,12 +1,12 @@
-# app/routes/fix_admin.py
+# app/routes/diagnose_admin.py
 from flask import Blueprint
 from app.models import User
 from werkzeug.security import generate_password_hash
 from app import db
 
-fix_admin_bp = Blueprint('fix_admin', __name__)
+diagnose_admin_bp = Blueprint('diagnose_admin', __name__)
 
-@fix_admin_bp.route('/fix-admin-password')
+@diagnose_admin_bp.route('/fix-admin-password')
 def fix_admin_password():
     try:
         admin = User.query.filter_by(username='admin').first()
